e2ba36a024fd54acfb07890e8df728c934bcbe7e
Tom Kennedy <holtsrule1@gmail.com>
2023-03-24 19:41:00 -0400

    Catalog Feature

--- src/Products/Products.tsx
+++ src/Products/Products.tsx
@@ -1,7 +1,27 @@
+import data from "./data";
+
 function Products() {
     return (
-        <div>
-            <h2>Products</h2>
+        <div className="content">
+            <ul className ="products">
+                {data.products.map((product) => (
+                    <li>
+                        <div className="product">
+                            <img
+                                className="product-image"
+                                src={product.imageUrl}
+                                alt="product"
+                            />
+                            <div className="product-name">
+                                <a href="product.html">{product.name}</a>
+                            </div>
+                            <div className="product-brand">{product.brand}</div>
+                            <div className="product-price">{product.price}</div>
+                            <div className="product-rating">{product.rating} Stars ({product.numberOfReviews} reviews)</div>
+                        </div>
+                    </li>
+                ))};
+            </ul>
         </div>
     );
 }
